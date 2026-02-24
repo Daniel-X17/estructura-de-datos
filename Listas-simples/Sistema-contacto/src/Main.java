426ae0c96dec09068fa178f7c57c959c7a61b046
DanielX17 <arenasbernatedaniel@gmail.com>
2026-02-24 07:55:12 -0500

    sistema de contactos usando listas simples

---
+++ Listas-simples/Sistema-contacto/src/Main.java
@@ -0,0 +1,12 @@
+public class contacto {
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public Contacto(String nombre, String telefono, String correo){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+
+    }
+}
